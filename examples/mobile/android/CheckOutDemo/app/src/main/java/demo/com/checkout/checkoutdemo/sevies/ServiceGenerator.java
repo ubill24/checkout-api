c2842b0acbde8c02cfc8b57148c65eb644790d5d
youraleangseng <youra.leangseng@gmail.com>
2018-08-16 08:49:25 +0700

    Change API_URL

--- examples/mobile/android/CheckOutDemo/app/src/main/java/demo/com/checkout/checkoutdemo/sevies/ServiceGenerator.java
+++ examples/mobile/android/CheckOutDemo/app/src/main/java/demo/com/checkout/checkoutdemo/sevies/ServiceGenerator.java
@@ -9,13 +9,11 @@ import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-/**
- * Created by srsc12 on 0006-06-Jan-01-2017.
- */
 
 public class ServiceGenerator {
 
-    public static final String API_BASE_URL = "https://checkoutapi-dev0.bill24.net/";
+    public static final String API_BASE_URL = "https://checkoutapi-demo.bill24.net/";
+    public static final String TOKEN = "a8024ffe355342ef890fcebed5ad3009";
 
     private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
@@ -32,7 +30,7 @@ public class ServiceGenerator {
                 Request original = chain.request();
 
                 Request.Builder requestBuilder = original.newBuilder()
-                        .header("token" , "c696e16967e242d9ae9a5bfb2de8867d")
+                        .header("token" , TOKEN)
                         .header("Content-Type" , "application/json")
                         .header("Accept" , "application/json")
                         .method(original.method() , original.body()) ;
