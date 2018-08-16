3331abbf3366e0684f9ab3331151e4ddc7539bed
youraleangseng <youra.leangseng@gmail.com>
2018-08-16 08:32:56 +0700

    Sample code ios swift

---
+++ examples/mobile/ios/CheckOut24/CheckOut24/Model/CheckOut.swift
@@ -0,0 +1,17 @@
+//
+//  CheckOut.swift
+//  CheckOut24
+//
+//  Created by Youra Leangseng on 8/8/18.
+//  Copyright © 2018 Youra Leangseng. All rights reserved.
+//
+
+import Foundation
+class CheckOut{
+    
+    let description:String? = nil
+    let currency:String? = nil
+    let amount:Float? = nil
+    let reference_id:String? = nil
+    let webview:Bool? = true
+}
